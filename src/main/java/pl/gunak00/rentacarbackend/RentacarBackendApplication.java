7a3fe4f1d9296e51963b78b9a2a36b443ab995b7
Gunak00 <pkonwa00@gmail.com>
2023-01-02 20:34:39 +0100

    Database configuration

---
+++ src/main/java/pl/gunak00/rentacarbackend/RentacarBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.gunak00.rentacarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentacarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentacarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gunak00/rentacarbackend/RentacarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gunak00.rentacarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
