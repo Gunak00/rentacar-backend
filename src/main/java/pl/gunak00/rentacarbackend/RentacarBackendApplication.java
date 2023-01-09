ecbd8a538d17ad625d5c9bd3fb2c3138e4c30cca
Gunak00 <pkonwa00@gmail.com>
2023-01-09 19:43:12 +0100

    Merged with corsConfiguration

--- src/main/java/pl/gunak00/rentacarbackend/RentacarBackendApplication.java
+++ src/main/java/pl/gunak00/rentacarbackend/RentacarBackendApplication.java
@@ -2,12 +2,7 @@ package pl.gunak00.rentacarbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 public class RentacarBackendApplication {
