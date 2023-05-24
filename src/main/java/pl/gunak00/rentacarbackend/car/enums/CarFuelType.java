c48fd040008c8db64106435dad3517a218099ef6
Gunak00 <pkonwa00@gmail.com>
2023-05-24 21:20:35 +0200

    HYBRID_DEISEL in CarFuelType.java deleted

--- src/main/java/pl/gunak00/rentacarbackend/car/enums/CarFuelType.java
+++ src/main/java/pl/gunak00/rentacarbackend/car/enums/CarFuelType.java
@@ -1,5 +1,5 @@
 package pl.gunak00.rentacarbackend.car.enums;
 
 public enum CarFuelType {
-    DIESEL, GASOLINE, GASOLINE_LPG, ELECTRIC, HYBRID_GASOLINE, HYBRID_DIESEL;
+    DIESEL, GASOLINE, GASOLINE_LPG, ELECTRIC, HYBRID_GASOLINE;
 }
